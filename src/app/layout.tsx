68b5cd80abb5bfe3e32325da15b8085724f5a579
wesselpawel <wesselpawel@users.noreply.github.com>
2023-10-30 15:25:51 +0100

    contact form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,28 +100,18 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${cocosharp.variable} bg-[#1d1d1d]`}>
         <Providers>{children}</Providers>
-
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=G-1CSY7FWMBY`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
-          id="gtm-script"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer', 'G-1CSY7FWMBY');
-            `,
-          }}
+          src="https://www.googletagmanager.com/gtag/js?id=GT-WRDF58Q"
         />
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GT-WRDF58Q');
+          `}
+        </Script>
       </body>
     </html>
   );
