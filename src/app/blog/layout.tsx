18b8643f9eb133d75c5e7ede51002e0e48f4e043
wesselpawel <wesselpawel@users.noreply.github.com>
2023-11-25 03:20:51 +0100

    shop cart validation

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import TopBar from "../shop/components/ShopHeader/TopBar";
 import Orders from "../shop/components/Orders";
 import ShopFooter from "../shop/components/ShopFooter";
+import PrepareCart from "../shop/components/PrepareCart";
 
 export default function ShopLayout({
   children,
@@ -11,6 +12,7 @@ export default function ShopLayout({
   return (
     <div className="bg-white overflow-hidden">
       {" "}
+      <PrepareCart />
       <TopBar />
       {children}
       <Orders />
